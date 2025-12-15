0751380dcce0d71697c10eac04f20351a03fbf21
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-12-15 12:44:28 +0530

    add: chat screen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar, useColorScheme } from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import LoginScreen from './src/screens/LoginScreen';
+import ChatScreen from './src/screens/ChatScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -9,7 +10,8 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <LoginScreen />
+      {/* <LoginScreen /> */}
+      <ChatScreen />
     </SafeAreaProvider>
   );
 }
---
+++ src/screens/ChatScreen/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ChatScreen = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>ChatScreen</Text>
+        </SafeAreaView>
+    );
+};
+
+export default ChatScreen;
