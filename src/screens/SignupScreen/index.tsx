59c11dd2883a13edc03b6c7649aa72ac3f0874cf
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-12-15 11:21:58 +0530

    initial commit

---
+++ src/screens/SignupScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+const SignupScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Signup Screen</Text>
+    </View>
+  );
+};
+
+export default SignupScreen;
